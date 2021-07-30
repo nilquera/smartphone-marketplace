f502cd1a2f61ffd15683fa6d9778b8880affa4b0
Nil Quera <nlquera97@gmail.com>
2021-07-30 12:08:30 +0200

    Add caching with expiration to shopping cart

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,9 +1,20 @@
+import { getObjectCached, setObjectCached } from "services";
+
 const { createContext, useState } = require("react");
 
 const Context = createContext({});
 
+const TTL = 1000 * 60;
+
 export function CartContextProvider({ children }) {
-  const [cartItems, setCartItems] = useState(0);
+  const [cartItems, setCartItemsState] = useState(
+    getObjectCached("cartItems") || 0
+  );
+
+  const setCartItems = (value) => {
+    setCartItemsState(value);
+    setObjectCached("cartItems", value, TTL);
+  };
 
   return (
     <Context.Provider value={{ cartItems, setCartItems }}>
